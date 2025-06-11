88ff348a3b586aed5c69bf7e3823a2ef2a2c2866
uniatrix <uniatrix@gmail.com>
2025-06-11 02:37:00 -0300

    fix: Update API endpoints and add Facebook Pixel tracking to Obrigado page
    Change the API endpoint in the ConsultaINSS component to a new URL for form submissions. Update the Content Security Policy in index.html to reflect the new endpoint. Additionally, implement Facebook Pixel tracking code in the Obrigado page to enhance analytics capabilities and user tracking.

--- src/pages/Obrigado.tsx
+++ src/pages/Obrigado.tsx
@@ -38,6 +38,26 @@ const Obrigado: React.FC = () => {
                     name="description"
                     content="Seu agendamento foi confirmado. Agradecemos sua confiança e preparamos informações importantes para você."
                 />
+                {/* Facebook Pixel Code */}
+                <script>
+                    {`
+                        !function(f,b,e,v,n,t,s)
+                        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                        n.queue=[];t=b.createElement(e);t.async=!0;
+                        t.src=v;s=b.getElementsByTagName(e)[0];
+                        s.parentNode.insertBefore(t,s)}(window, document,'script',
+                        'https://connect.facebook.net/en_US/fbevents.js');
+                        fbq('init', '1748150686076875');
+                        fbq('track', 'PageView');
+                        fbq('track', 'Lead');
+                    `}
+                </script>
+                <noscript>
+                    {`<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1748150686076875&ev=PageView&noscript=1" />`}
+                </noscript>
+                {/* End Facebook Pixel Code */}
             </Helmet>
 
             <div className="min-h-screen bg-gradient-to-br from-law-black via-law-black-light to-law-black text-law-white py-20 sm:py-24 px-4 sm:px-6 lg:px-8">
